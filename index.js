82238af17b8513b0155dc263ea053fe32ce39cfa
Kirill Neruchev <kirill@neruchev.com>
2024-07-29 13:04:02 +0200

    Prettify codebase

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 module.exports = {
   rules: {
-    "no-full-import-of-libraries": require("./rules/no-full-import-of-libraries"),
-    "no-incorrect-paths-in-dictionary-keys": require("./rules/no-incorrect-paths-in-dictionary-keys"),
-    "no-index-imports": require("./rules/no-index-imports"),
-    "no-selective-import-of-libraries": require("./rules/no-selective-import-of-libraries"),
-    "no-unused-dictionary-keys": require("./rules/no-unused-dictionary-keys"),
+    'no-full-import-of-libraries': require('./rules/no-full-import-of-libraries'),
+    'no-incorrect-paths-in-dictionary-keys': require('./rules/no-incorrect-paths-in-dictionary-keys'),
+    'no-index-imports': require('./rules/no-index-imports'),
+    'no-selective-import-of-libraries': require('./rules/no-selective-import-of-libraries'),
+    'no-unused-dictionary-keys': require('./rules/no-unused-dictionary-keys'),
   },
   configs: {
-    recommended: require("./configs/recommended"),
+    recommended: require('./configs/recommended'),
   },
 };
